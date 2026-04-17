10ed779b15400d67f2aec88c4064930354905de7
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-17 19:17:51 +0200

    Remove manifest_load() and manifest_local_copy() functions

--- src/dataframe.py
+++ src/dataframe.py
@@ -2,32 +2,6 @@ import pandas as pd
 from pathlib import Path
 
 
-def manifest_load(manifest_path: str | Path) -> pd.DataFrame:
-    # TODO refactor original manifest variable: not clear which manifest
-    """
-    This function loads the original manifest file.
-
-    Args:
-        manifest_path (Path): Path to the manifest file.
-    """
-    return pd.read_parquet(manifest_path)
-
-
-def manifest_local_copy(
-        manifest_path: str | Path,
-        dst_path: str | Path
-) -> None:
-    """
-    This function creates a local copy of the manifest file.
-
-    Args:
-        manifest_path (Path): Path to the original manifest file.
-        dst_path (Path): Path to the manifest file copy.
-    """
-    df = pd.read_parquet(manifest_path)
-    df.to_parquet(dst_path, index=False)
-
-
 def dataframe_split(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     This function splits a dataframe into two: one for training and one for validation, based on
