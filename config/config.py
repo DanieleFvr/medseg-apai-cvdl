f933b7fa96656c99ead9060b6cfca01619157065
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-17 19:29:12 +0200

    Add config/config.py file with path variables already included

---
+++ config/config.py
@@ -0,0 +1,5 @@
+# Paths
+manifest_path = Path(f"/content/drive/MyDrive/APAI_CVDL_shared/project/meta/manifest.parquet")
+LOCAL_ROOT = Path("/content/project_data/data/pre/img/")
+ROOT = Path("/content/")
+LOCAL_MANIFEST = Path(f"{ROOT}/manifest_local.parquet")
