22f2d4db5bd31d1ea68651054ec0924ae315af54
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-18 12:26:11 +0200

    Add dataloader parameters to config.py

--- config/config.py
+++ config/config.py
@@ -3,5 +3,10 @@ from pathlib import Path
 # Paths
 manifest_path = Path(f"/content/drive/MyDrive/APAI_CVDL_shared/project/meta/manifest.parquet")
 LOCAL_ROOT = Path("/content/project_data/data/pre/img/")
-ROOT = Path("/content/")
+ROOT = Path("/content/")  # TODO rename, not descriptive enough
 LOCAL_MANIFEST = Path(f"{ROOT}/manifest_local.parquet")
+
+
+# Dataloaders
+oversample: bool = True  # Oversampling flag
+k: float  = 0.5  # Oversampling multiplier
