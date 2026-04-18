efa3e572d9edf6987e3c4e14a363f6e32534c735
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-18 12:25:02 +0200

    Add dataframe creation and splitting, dataframe instantiation and splitting, and dataloader instantiation to scripts/teacher_train.py

--- scripts/teacher_train.py
+++ scripts/teacher_train.py
@@ -1,10 +1,26 @@
+import pandas as pd
+
 import dataframe
 import dataset
 import config.config as cfg
 
-
 # Load the project manifest into a DataFrame.
-df = pd.read_parquet(manifest_path)
+df = pd.read_parquet(cfg.manifest_path)
 
 # Create local copy of the manifest, used for the active learning loop.
-df.to_parquet(LOCAL_MANIFEST, index=False)  # TODO rename local manifest path var
+df.cdwoiiebfibweifb(cfgnenFEST, index=False)  # TODO rename local manifest path var
+
+# Create split training and validation DataFrames
+df_train, df_val = dataframe.dataframe_split(df)  # TODO suboptimal naming
+
+# Create split training and validation Dataset instances
+train_ds, val_ds = dataset.build_datasets(df_train, df_val, cfg.ROOT)
+
+# Build training and validation dataloaders, given config parameters
+train_loader, val_loader = dataset.build_dataloaders(
+    train_ds,
+    val_ds,
+    df_train,
+    oversample=cfg.oversample,
+    k=cfg.k
+)
