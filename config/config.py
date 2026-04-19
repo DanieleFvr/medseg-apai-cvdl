944f0e020889c9c982f79870f06b0068618a9659
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-19 19:14:41 +0200

    Add config values to config.py (SSL, device, AL sample selection, teacher model)

--- config/config.py
+++ config/config.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 
-# Paths
-manifest_path = Path(f"/content/drive/MyDrive/APAI_CVDL_shared/project/meta/manifest.parquet")
-LOCAL_ROOT = Path("/content/project_data/data/pre/img/")
-ROOT = Path("/content/")  # TODO rename, not descriptive enough
-LOCAL_MANIFEST = Path(f"{ROOT}/manifest_local.parquet")
+import torchvision.transforms as T
 
+# Paths
+manifest_path: str | Path = Path(f"/content/drive/MyDrive/APAI_CVDL_shared/project/meta/manifest.parquet")
+LOCAL_ROOT: str | Path = Path("/content/project_data/data/pre/img/")  # TODO should be lowercase
+ROOT: str | Path = Path("/content/")  # TODO rename, not descriptive enough, and should be lowercase
+LOCAL_MANIFEST: str | Path = Path(f"{ROOT}/manifest_local.parquet")  # TODO should be lowercase
 
 # Dataloaders
 oversample: bool = True  # Oversampling flag
 k: float  = 0.5  # Oversampling multiplier
+
+# Rotation prediction SSL
+center_crop = T.CenterCrop(size=384) # TODO find data type
+
+# Device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Active learning sample selection
+K: int = 800
+batch_size: int = 8
+
+# Teacher model
+num_groups: int = 8  # Number of groups for GroupNorm
