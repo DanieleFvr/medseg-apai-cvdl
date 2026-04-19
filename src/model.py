815f3fe6b19dd2e6b388babb682fce0eb807d8fc
Daniele <192154140+DanieleFvr@users.noreply.github.com>
2026-04-19 19:03:44 +0200

    Extract double_convolution() from src/model.py into src/layers.py to remove circular dependency with src/ssl_rotation_prediction.py

--- src/model.py
+++ src/model.py
@@ -1,41 +1,10 @@
 import torch.nn as nn
 
+from layers import double_convolution
 
-def double_convolution(
-        in_channels,
-        out_channels,
-        num_groups=8
-) -> nn.Sequential:
+class UNet(nn.Module):  # TODO refine class description
     """
-    This function creates two consecutive convolution layers.
-
-    Args:
-        in_channels (int): number of input channels.
-        out_channels (int): number of output channels.
-        num_groups (int): number of groups for GroupNorm.
-    Returns:
-        conv_op (nn.sequential): two convolution layers.
-    """
-    # for GroupNorm, ensure num_groups divides out_channels
-    g = min(num_groups, out_channels)
-    while out_channels % g != 0:
-        g -= 1
-
-    conv_op = nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-        nn.GroupNorm(g, out_channels),  # GroupNorm
-        nn.ReLU(inplace=True),
-
-        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-        nn.GroupNorm(g, out_channels),  # GroupNorm
-        nn.ReLU(inplace=True)
-    )
-    return conv_op
-
-
-class UNet(nn.Module): # TODO refine class description
-    """
-    This class defines the U-Net architecture.
+    This class defines the student model architecture.
     """
 
     def __init__(self, num_classes):
